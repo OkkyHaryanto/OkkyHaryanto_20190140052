59687e3ef86a930775708297a4d7497ff8e83c3d
OkkyHaryanto <63486943+OkkyHaryanto@users.noreply.github.com>
2021-03-24 00:22:54 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+/* Mobile */    
+    .bagian-atas{
+    text-align: center;
+    }
+
+    .bagian-bawah{
+    display: flex;
+    justify-content: center;
+    }
+/* Tablet */
+@media (min-width: 768px) and (max-width: 991.98px) {
+    .bagian-atas{
+        color: greenyellow;
+    }
+
+}
+/* Layar PC */
+@media (min-width: 992px) {
+
+    .bagian-atas{
+        font-size: 50px;
+        color: red;
+    }
+
+    .bagian-bawah{
+        display: flex;
+        justify-content: start;
+    }
+
+
+}
